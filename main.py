4237149bb5c977840725cfd47420287eeb988f5c
git-MUSE <69621473+git-MUSE@users.noreply.github.com>
2021-02-01 02:16:14 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+mus= os.listdir(path='.')
+
+with open('test.m3u', 'w') as file:
+    file.write(r'#EXTM3U'+'\n')
+
+    for i in mus:
+        if i=='test.m3u' or i=='main.py':
+            pass
+        else:
+            file.write(r'#EXTINF:1,'+i+'\n')
+            file.write(i+'\n'+'\n')
+
+os.startfile('test.m3u')
+
+
+
+
+
+
+
+
+
+
